c2cf5b0c9bd3b8b0e4ca0fbcb82644c1e50cafff
JuliaVal821 <julia.kolgatina@gmail.com>
2020-11-07 22:16:51 -0500

    Wrote a function that produces an array with the numbers 0 to N-1 in it.

---
+++ Filling an array.js
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
+
+    function arr (N) {
+        let array = [];
+        for(let i = 0; i <= N - 1; i++){
+            array.push(i)
+        }
+        return array;
+    }
